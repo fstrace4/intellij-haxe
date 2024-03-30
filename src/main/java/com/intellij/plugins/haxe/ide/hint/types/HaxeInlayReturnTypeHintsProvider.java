bea7f204436c59ccb8b256aafe57f55bea32c377
m0rkeulv <github@m0rkeulv.net>
2024-03-30 21:45:22 +0100

    adding return type inlay for local functions

--- src/main/java/com/intellij/plugins/haxe/ide/hint/types/HaxeInlayReturnTypeHintsProvider.java
+++ src/main/java/com/intellij/plugins/haxe/ide/hint/types/HaxeInlayReturnTypeHintsProvider.java
@@ -2,10 +2,10 @@ package com.intellij.plugins.haxe.ide.hint.types;
 
 import com.intellij.codeInsight.hints.declarative.*;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.plugins.haxe.lang.psi.HaxeLocalFunctionDeclaration;
 import com.intellij.plugins.haxe.lang.psi.HaxeMethodDeclaration;
 import com.intellij.plugins.haxe.model.HaxeMethodModel;
-import com.intellij.plugins.haxe.model.type.HaxeGenericResolver;
-import com.intellij.plugins.haxe.model.type.ResultHolder;
+import com.intellij.plugins.haxe.model.type.*;
 import com.intellij.plugins.haxe.util.UsefulPsiTreeUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -28,6 +28,25 @@ public class HaxeInlayReturnTypeHintsProvider implements InlayHintsProvider {
     public void collectFromElement(@NotNull PsiElement element, @NotNull InlayTreeSink sink) {
       if (element instanceof HaxeMethodDeclaration fieldDeclaration) {
         handleMethodDeclarationHints(sink, fieldDeclaration);
+      }else if (element instanceof HaxeLocalFunctionDeclaration functionDeclaration) {
+        handleFunctionDeclarationHints(sink, functionDeclaration);
+      }
+    }
+
+    private void handleFunctionDeclarationHints(InlayTreeSink sink, HaxeLocalFunctionDeclaration declaration) {
+
+      if (declaration.getTypeTag() == null) {
+        SpecificFunctionReference functionReference = HaxeTypeResolver.getPsiElementType(declaration, null).getFunctionType();
+        if (functionReference != null) {
+          ResultHolder returnType = functionReference.getReturnType();
+          PsiElement paramListEnd = UsefulPsiTreeUtil.getNextSiblingSkipWhiteSpacesAndComments(declaration.getParameterList());
+          if (paramListEnd == null) return;
+          int offset = paramListEnd.getTextRange().getEndOffset();
+          if (!returnType.isUnknown() && !returnType.getType().isInvalid()) {
+            InlineInlayPosition position = new InlineInlayPosition(offset, false, 0);
+            sink.addPresentation(position, null, null, false, appendTypeTextToBuilder(returnType));
+          }
+        }
       }
     }
 
