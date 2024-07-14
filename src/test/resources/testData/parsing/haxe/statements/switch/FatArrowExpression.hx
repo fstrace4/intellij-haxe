dbdc4134a9dc720e96ac5f368ee31e07fa135601
m0rkeulv <github@m0rkeulv.net>
2024-07-14 19:42:31 +0200

    allow switchExtractorExpressions to be wrapped in parentheses

--- src/test/resources/testData/parsing/haxe/statements/switch/FatArrowExpression.hx
+++ src/test/resources/testData/parsing/haxe/statements/switch/FatArrowExpression.hx
@@ -4,6 +4,10 @@ class FatArrowExpression {
     var a = map1[0];
     var b:Int = 0;
 
+    //TODO mlo: not sure if any of this makes any sence now?
+    // fatArrow expressions are only used in map inits right?
+    // these looks more like extractor expressons
+
     switch a
     {
       // This should parse as a fatArrowExpression.
